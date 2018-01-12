e6105660e7066c387a5801e65246a59cea949815
Kristin Brandenburg <kristinburg@gmail.com>
2018-01-12 17:35:37 +0100

    refactor url function

--- cryptohistory/cryptohistory.py
+++ cryptohistory/cryptohistory.py
@@ -13,8 +13,7 @@ def generate_url_by_dates(timestamp_start, timestamp_end):
     starttime = convert_datetime_to_timestamp(timestamp_start)
     endtime = convert_datetime_to_timestamp(timestamp_end)
     url = 'https://graphs.coinmarketcap.com/currencies/bitcoin/'
-    return url + str(starttime) + '/' + str(endtime)
-
+    return (f'{url}{str(starttime)}/{str(endtime)}')
 
 print(convert_datetime_to_timestamp('2017-05-10 23:57:44'))
 print(convert_datetime_to_timestamp('2017-6-15 23:57:44'))
