192bde95836bd597e9da88514f584f0c79625325
Kristin Brandenburg <kristinburg@gmail.com>
2018-01-12 17:25:26 +0100

    convert datetime string to timestamp

--- cryptohistory/cryptohistory.py
+++ cryptohistory/cryptohistory.py
@@ -1,3 +1,16 @@
-import os, datetime
+import time
+from datetime import datetime
 
-print("Hello World")
+# convert datetime to unix timestamp
+# 2018-01-31 01:58:11:00
+
+# convert string date to datetime object
+def convert_datetime_to_timestamp(string_datetime):
+    converted_datetime = datetime.strptime(string_datetime, '%Y-%m-%d %H:%M:%S')
+    dt = time.mktime(converted_datetime.timetuple())
+    return int(dt) * 1000
+
+print(convert_datetime_to_timestamp('2016-05-10 23:57:44'))
+print(convert_datetime_to_timestamp('2016-6-15 23:57:44'))
+
+# https://graphs.coinmarketcap.com/currencies/bitcoin/n1/n2/
