9a1f0f58f7729cd0a40d2238a1457ea668628fa4
Kristin Brandenburg <kristinburg@gmail.com>
2018-01-15 14:26:47 +0100

    writes url data to csv

--- cryptohistory/cryptohistory.py
+++ cryptohistory/cryptohistory.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime, timedelta
 import requests
+import csv
 from date_utils import (
     datetime_to_timestamp, stringdate_to_datetime,
     stringdate_to_string_datetime, validate_date_string
@@ -15,16 +16,25 @@ def generate_bitcoin_url(start_datetime_obj, end_datetime_obj):
 
 
 def get_crypto_historical_data(string_startdate, string_enddate):
-    sd = stringdate_to_datetime(string_startdate)
-    ed = stringdate_to_datetime(string_enddate)
-    delta = timedelta(hours=1)
-    while sd <= ed:
-        print(generate_bitcoin_url(sd, sd+delta))
-        sd += delta
+    # sd = stringdate_to_datetime(string_startdate)
+    # ed = stringdate_to_datetime(string_enddate)
+    # delta = timedelta(hours=1)
+    # url = (generate_bitcoin_url(sd, sd+delta))
+    with open('output.csv', 'w', 1) as output:
+        sd = stringdate_to_datetime(string_startdate)
+        ed = stringdate_to_datetime(string_enddate)
+        delta = timedelta(hours=1)
+        url = (generate_bitcoin_url(sd, sd+delta))
+        while sd <= ed:
+            output.write(url + '\n')
+            # import pdb ; pdb.set_trace()
+            print(url)
+            sd += delta
+        output.close()
 
 
 x = '2017-12-01 00:00:00'
-y = '2017-12-08 13:00:00'
+y = '2017-12-02 13:00:00'
 print(get_crypto_historical_data(x, y))
 
 
