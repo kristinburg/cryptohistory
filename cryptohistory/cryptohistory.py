53e7c2a3ca4cbe606b41db9aca23998ea3fc511a
Kristin Brandenburg <kristinburg@gmail.com>
2018-01-16 21:05:27 +0100

    tests for csv rows function pass

--- cryptohistory/cryptohistory.py
+++ cryptohistory/cryptohistory.py
@@ -1,6 +1,7 @@
 import time
 from datetime import datetime, timedelta
 import requests
+import json
 import csv
 from date_utils import (
     datetime_to_timestamp, stringdate_to_datetime,
@@ -15,20 +16,49 @@ def generate_bitcoin_url(start_datetime_obj, end_datetime_obj):
     return f'{url}/{start_timestamp}/{end_timestamp}/'
 
 
+def generate_ethereum_url(start_datetime_obj, end_datetime_obj):
+    start_timestamp = datetime_to_timestamp(start_datetime_obj)
+    end_timestamp = datetime_to_timestamp(end_datetime_obj)
+    url = 'https://graphs.coinmarketcap.com/currencies/ethereum'
+    return f'{url}/{start_timestamp}/{end_timestamp}/'
+
+
+def response_to_csv_rows(json_response):
+    # takes json_response and returns csv rows
+    market_caps = json_response['market_cap_by_available_supply']
+    price_btcs = json_response['price_btc']
+    price_usds = json_response['price_usd']
+    volume_usds = json_response['volume_usd']
+    csv_rows = []
+
+    for i, market_cap in enumerate(market_caps):
+        timestamp = market_cap[0]
+        market_cap_value = market_cap[1]
+        price_btc_value = price_btcs[i][1]
+        price_usd_value = price_usds[i][1]
+        volume_usd_value = volume_usds[i][1]
+        csv_row = [timestamp, market_cap_value, price_btc_value,
+                   price_usd_value, volume_usd_value]
+        csv_rows.append(csv_row)
+    return csv_rows
+
+
 def get_crypto_historical_data(string_startdate, string_enddate):
     with open('output.csv', 'w', 1) as output:
-        schema = ['timestamp','market_cap','price_usd','price_btc', 'volume']
+        csv_headers = ['timestamp','market_cap','price_btc','price_usd', 'volume']
         writer = csv.writer(output)
-        # Gives the header name row into csv
-        writer.writerow([g for g in schema])
+        writer.writerow([g for g in csv_headers])
 
         sd = stringdate_to_datetime(string_startdate)
         ed = stringdate_to_datetime(string_enddate)
         delta = timedelta(hours=1)
         url = generate_bitcoin_url(sd, sd+delta)
+
         while sd <= ed:
             response = requests.get(url)
-            output.write(response.text + '\n')
+            data = response.json()
+            csv_row = response_to_csv_rows(data)
+            output.write(csv_row + '\n')
             sd += delta
         output.close()
 
@@ -38,7 +68,7 @@ if __name__ == '__main__':
     Only run this code when explicitly calling it. (not via import)
     """
     x = '2017-12-01 00:00:00'
-    y = '2017-12-01 6:00:00'
+    y = '2017-12-01 2:00:00'
 
     get_crypto_historical_data(x, y)
     # r = requests.get(url)
--- cryptohistory/tests/test_cryptohistory.py
+++ cryptohistory/tests/test_cryptohistory.py
@@ -1,5 +1,31 @@
-# from cryptohistory import generate_bitcoin_url
+from cryptohistory import response_to_csv_rows
 
 
-def test_generate_bitcoin_url():
-    pass
+def test_response_to_csv_rows():
+    example_response = {
+        "market_cap_by_available_supply":
+            [
+                [1512083052000, 168480198487],
+                [1512083353000, 169212161618],
+            ],
+        "price_btc":
+            [
+                [1512083052000, 1.0],
+                [1512083353000, 1.0],
+            ],
+        "price_usd":
+            [
+                [1512083052000, 10081.7],
+                [1512083353000, 10125.5],
+            ],
+        "volume_usd":
+            [
+                [1512083052000, 8438670000],
+                [1512083353000, 8478910000],
+            ]
+    }
+
+    assert response_to_csv_rows(example_response) == [
+        [1512083052000, 168480198487, 1.0, 10081.7, 8438670000],
+        [1512083353000, 169212161618, 1.0, 10125.5, 8478910000],
+    ]
