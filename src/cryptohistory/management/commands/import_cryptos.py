7f1c96c7d8314fdaafff80f48a62277bbef0e236
Michael van de Waeter <mvdwaeter@gmail.com>
2018-01-18 13:44:17 +0100

    import BTC

--- src/cryptohistory/management/commands/import_cryptos.py
+++ src/cryptohistory/management/commands/import_cryptos.py
@@ -2,16 +2,16 @@ from datetime import timedelta
 
 from django.core.management.base import BaseCommand
 
-from cryptohistory.utils import ETH, get_crypto_historical_data
+from cryptohistory.utils import BTC, get_crypto_historical_data
 
 
 class Command(BaseCommand):
     help = 'Import cryptos'
 
     def handle(self, *args, **options):
-        start_date = '2017-03-13'
+        start_date = '2013-04-28'
         end_date = '2018-01-18'
-        currency = ETH
+        currency = BTC
         delta = timedelta(days=1)
 
         get_crypto_historical_data(start_date, end_date, delta, currency)
